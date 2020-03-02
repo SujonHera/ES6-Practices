955bbbc12281edf1b38150d8bacc306115c224f6
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-03 00:22:51 +0600

    let varialbe  value can be change in a scope but const variable value cann't change:

---
+++ letConst.js
@@ -0,0 +1,15 @@
+const habby = "My Habi";
+console.log(habby);
+
+const numbers = [23, 24];
+numbers.push(29);
+console.log(numbers[0] = 322);
+
+console.log(numbers);
+
+// let variable can be change value in a scope
+let sum  = 0;
+for(let i  = 0; i <= 10; i++) {
+    sum = sum + i;
+}
+console.log(sum)
