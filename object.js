ee72ed2cc188c4bc88d1d55c45fd11389c53c8b7
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-04 06:01:14 +0600

    wow i am happy now because i am learning javascript programming language but i don't know properly most important concept

---
+++ object.js
@@ -0,0 +1,16 @@
+let name = "Sujon Hossain";
+let email = "sujon.hossain758@gmail.com";
+
+const information = {
+    name, 
+    email,
+    print(){
+        console.log(`
+           Name: ${this.name}
+           Email: ${this.email}
+        `);
+    }
+}
+information.print()
+
+// wow i am happy now because i am learning javascript programming language but i don't know properly most important concept;
